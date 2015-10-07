fab859c5682f1642b80a0d9deecf8b7f43186624
Paul Hamby <paul.hamby@bancvue.com>
2015-10-06 22:51:05 -0500

    refactor, add retry option, add login/logout functions

--- bin/check-dyn-qps.rb
+++ bin/check-dyn-qps.rb
@@ -26,7 +26,6 @@
 #   for details.
 
 require 'sensu-plugin/check/cli'
-#require 'rest-client'
 require 'net/https'
 require 'uri'
 require 'json'
@@ -78,6 +77,13 @@ class CheckDynQPS < Sensu::Plugin::Check::CLI
 		description: 'Warning threshold. This could be close to your query allotment from Dyn',
 		required: true
 
+	option :retries,
+		short: '-r',
+		long: '--retries 100',
+		description: 'The number of times we will retry the request when we receive a 301,302 or 307. You want this to be pretty high',
+		default: 50
+
+
 
 	def run
 		if config[:period] == "day"
@@ -87,78 +93,89 @@ class CheckDynQPS < Sensu::Plugin::Check::CLI
 		else
 			unknown "Valid options for period are day or week"
 		end
-
 		end_ts = (Time.now).to_i
-
 		url = URI.encode(config[:url])
+
+		auth_token = login(url)
+
+		qpsreport_url = URI.parse(url) + "QPSReport/"
+		headers = { "Content-Type" => 'application/json', 'Auth-Token' => auth_token }
+		http = Net::HTTP.new(qpsreport_url.host, qpsreport_url.port)
+		http.use_ssl = true
+		parameters = { :start_ts => start_ts, :end_ts => end_ts }
+		response = http.post(qpsreport_url.path, parameters.to_json, headers)
+		$try = 0
+		$retries = config[:retries]
 		begin
-			login_url = URI.parse(url) + "Session/"
-			headers = { "Content-Type" => 'application/json' }
-			http = Net::HTTP.new(login_url.host, login_url.port)
-			#http.set_debug_output $stderr
-			http.use_ssl = true
-
-			session_data = { :customer_name => config[:customer], :user_name => config[:user], :password => config[:password] }
-			response = http.post(login_url.path, session_data.to_json, headers)
-
-			result = JSON.parse(response.body)
-			auth_token = result['data']['token']
-
-			qpsreport_url = URI.parse(url) + "QPSReport/"
-			headers = { "Content-Type" => 'application/json', 'Auth-Token' => auth_token }
-			parameters = { :start_ts => start_ts, :end_ts => end_ts }
-			response = http.post(qpsreport_url.path, parameters.to_json, headers)
-			$try = 0
-			$retries = 100
-			begin
-				if response.code == "200"
-					break
-				elsif ["301", "302", "307"].include? response.code
-					redirected_url = response["location"]
-					puts("Redirected to #{redirected_url}")
-					puts("Retrying the request. This is try #{$try} of #{$retries}")
-					response = http.get(redirected_url, headers)	
-					$try +=1
-					sleep 5
-			  else
-					critical "Could not complete request after #{$try} retries\n"
-				end
-			end while $try < $retries
-
-			result = JSON.parse(response.body)
-			csv = result['data']['csv']
-
-			values = []
-			CSV.parse(csv) do |row|
-				q=row[1]
-				q=q.to_i
-				if q.is_a?(Integer)
-					#The data is in 300 second increments. Divide by 300 to get the amount of queries per second.
-					q=(q/300)
-					values.push(q)
-				end
-			end
-
-
-			values.shift
-			p = 0.95
-			percent = percentile(values,p)
-			puts percent
-
-			if percent >= config[:critical].to_i
-				percent = percent.to_s
-				critical "DynQPS is critical. QPS is #{percent} of #{config[:critical]}"
-			elsif percent >= config[:warning].to_i
-				percent = percent.to_s
-				warning "DynQPS is warning #{percent} of #{config[:warning]}"
+			if response.code == "200"
+				break
+			elsif ["301", "302", "307"].include? response.code
+				redirected_url = response["location"]
+				puts("Redirected to #{redirected_url}")
+				puts("Retrying the request. This is try #{$try} of #{$retries}")
+				response = http.get(redirected_url, headers)	
+				$try +=1
+				sleep 5
 			else
-				percent = percent.to_s
-				ok "DynQPS is ok #{percent} of #{config[:warning]}"
+				critical "Could not complete request after #{$try} retries\n"
 			end
+		end while $try < $retries
 
-			# Logout
-			response = http.delete(login_url.path, headers)
+		result = JSON.parse(response.body)
+		csv = result['data']['csv']
+
+		values = []
+		CSV.parse(csv) do |row|
+			q=row[1]
+			q=q.to_i
+			if q.is_a?(Integer)
+				#The data is in 300 second increments. Divide by 300 to get the amount of queries per second.
+				q=(q/300)
+				values.push(q)
+			end
 		end
+
+		values.shift
+		p = 0.95
+		percent = percentile(values,p)
+		puts percent
+
+		if percent >= config[:critical].to_i
+			percent = percent.to_s
+			critical "DynQPS is critical. QPS is #{percent} of #{config[:critical]}"
+		elsif percent >= config[:warning].to_i
+			percent = percent.to_s
+			warning "DynQPS is warning #{percent} of #{config[:warning]}"
+		else
+			percent = percent.to_s
+			ok "DynQPS is ok #{percent} of #{config[:warning]}"
+		end
+
+		logout
+
+	rescue SocketError, Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+		critical "#{config[:url]} is not responding. #{e}"
+	end
+
+	def login(url)
+		login_url = URI.parse(url) + "Session/"
+		headers = { "Content-Type" => 'application/json' }
+		http = Net::HTTP.new(login_url.host, login_url.port)
+		http.use_ssl = true
+
+		session_data = { :customer_name => config[:customer], :user_name => config[:user], :password => config[:password] }
+		response = http.post(login_url.path, session_data.to_json, headers)
+
+		result = JSON.parse(response.body)
+		auth_token = result['data']['token']
+		return auth_token
+
+	rescue SocketError, Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
+		critical "#{config[:url]} is not responding. #{e}"
+	end
+
+	def logout
+		http.delete(login_url.path, headers)
 	rescue SocketError, Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError, Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
 		critical "#{config[:url]} is not responding. #{e}"
 	end
